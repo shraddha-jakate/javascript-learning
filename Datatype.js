5de610bf227d578b65608ef36c8f505a3c0adb0a
Shraddha Jakate <shraddha.jakate@gmail.com>
2023-11-16 12:14:38 +0530

    Datatypes.js

---
+++ Datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all javascript codes as new version 
+
+// alert (3+3)  // we are using nodejs ,not browser so it wont run
+
+console.log(3+3);
+
+console.log("Shraddha")
+
+let name = "shraddha"
+let age = 19
+let state;  //undefined value yet 
+let city = null //empty 
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone/undefined value
+// symbol => unique
+
+
+//object 
+
+console.log(typeof "shraddha")
+
+console.log(typeof undefined); // undefined
+console.log(typeof nulll);  //object
+
