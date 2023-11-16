8be2cf9dff6b75a622dc0fd53697eeae48a1499e
Shraddha Jakate <shraddha.jakate@gmail.com>
2023-11-16 12:48:47 +0530

    conversationoperation.js

---
+++ conversationoperation.js
@@ -0,0 +1,21 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+onsole.log(typeof score);
+onsole.lpg(valueInNumber);
+
+"33" => 33
+ "33abc" => NaN
+ true => 1; fallse => 
+
+
+let isLoggedIn = 1
+
+let booleansIsLoggesIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggesIn);
+
+
