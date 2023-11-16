c6ec01465338a5ed7e948f3f960a408da14c8aa5
Shraddha Jakate <shraddha.jakate@gmail.com>
2023-11-16 14:49:55 +0530

    conversationoperations.js

--- conversationoperation.js
+++ conversationoperation.js
@@ -5,8 +5,8 @@ console.log(typeof(score));
 
 
 let valueInNumber = Number(score)
-onsole.log(typeof score);
-onsole.lpg(valueInNumber);
+console.log(typeof score);
+console.lpg(valueInNumber);
 
 "33" => 33
  "33abc" => NaN
@@ -17,5 +17,43 @@ let isLoggedIn = 1
 
 let booleansIsLoggesIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggesIn);
+
+
+let value = 3 
+let negvalues = -value
+console.log(negvalues);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+
+
+let str1 = "hello"
+let str2 = " shraddha"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+
+// console.log("2" > 1);
+// console.log("02">1);
+
+
+// console.log(null > 0);
+// console.log(null ==0);
+// console.log(null >= 0);
+
+
+//console.log(undefined == 0);
+//console.log(undefined < 0);
+//console.log(undefined < 0);
+
+// ===
+
+//console.log("2" ===2);
+
 
 
