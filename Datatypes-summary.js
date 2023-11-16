edc467378641b7f7bd8cf023f75732f29ddb773c
Shraddha Jakate <shraddha.jakate@gmail.com>
2023-11-16 17:37:18 +0530

    datatype summary

---
+++ Datatypes-summary.js
@@ -0,0 +1,40 @@
+// Primitive
+
+// 7 Types : String, Num, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100 
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;   //; this is undefined only
+
+const id = Symbol('123'
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//const bigNumber = 12354785695123
+
+
+
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+
+const heros = ["shaktiman", "naagraj"]
+{
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+
+}
+
+
+console.log(typeof bigNumber);
+
