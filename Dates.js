181ffa246ed4f5a35f046bb768532e079c90142d
Shraddha Jakate <shraddha.jakate@gmail.com>
2023-11-27 14:52:38 +0530

    dates.js

---
+++ Dates.js
@@ -0,0 +1,12 @@
+// Dates 
+
+let my Date = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2023,0,23)
+
+console.log(myCreatedDate.toDateString());
