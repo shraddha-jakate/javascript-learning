32bcafe0fb51b889ab04f5b9de0943ff6c38b38a
Shraddha Jakate <shraddha.jakate@gmail.com>
2023-12-05 11:51:47 +0530

    Forloop.js

--- Objects2.js
+++ Objects2.js
@@ -1,1 +1,27 @@
-Objects2.js
+// const tinderUser = new Object()
+const tinderuser = {}
+
+tinderuser.id = "123abc"
+tinderuser.name ="Sammy"
+tinderuser.isLoggedIn = false 
+
+console.log(tinderUser);
+
+const regularUset = {
+    email: "shrads@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "shraddha"
+            lastname:"choudhary"
+        }
+    }
+}
+
+//console.log(regularuser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj1 = [3:"a", 4:"b"}
+
+//const obj3 = {obj1, obj2}
+const obj3 = Object.assign(obj1, obj2)
+console.log(obj3);
