20aa90887b3a9ddec52908dd592abc4b264f38c5
Shraddha Jakate <shraddha.jakate@gmail.com>
2023-12-05 13:04:53 +0530

    whiledo.js

---
+++ whiledo.js
@@ -0,0 +1,34 @@
+
+
+while (index <= 10) {
+    console.log('Value of index is ${index}');
+    index=index + 2
+
+}
+let myArray = ['flash','batman','superman']
+
+
+let arr = 0
+while (arr < Array.legth){
+   //console.log('value is$(myArray[f]arr')
+};
+arr = arr+1
+}
+
+do{
+//console.log('Score')
+
+}
+while (score <= 10)
+
+
+let score = 11
+
+do{
+    console.log('Score is ${score}');
+    score++
+} while (score <= 10);
+
+
+
+
