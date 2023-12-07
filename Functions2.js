b7e00fcec64e0ded398037e5660c5bb202cea21c
Shraddha Jakate <shraddha.jakate@gmail.com>
2023-12-07 11:04:18 +0530

    Functions2.js

--- Functions2.js
+++ Functions2.js
@@ -1,6 +1,40 @@
-function calculateCartPrice(num1){
-    return num1
-  
-  }
-  
-  console.log(calculateCartPrice(2))
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1;
+}
+
+// console.log(calculateCartPrice(200, 500, 2000));
+
+const user = {
+    username: "shraddha",
+    price: 199
+};
+
+function handleObject(anyobject) {
+    console.log('Username is ' + anyobject.username + ' and price is ' + anyobject.price);
+}
+
+handleObject(user);
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));
+
+
+//console.log("Result;",result);
+
+function loginUserMessage(username = "shraddha"){
+    if(!username){
+        console.log("Please enter a username");
+        return
+
+return '${username} just logges in'
+}
+
+
+//console.log(lofinUserMessage("shraddha"));
+//console.log(loginUsermessage("shraddha"));
